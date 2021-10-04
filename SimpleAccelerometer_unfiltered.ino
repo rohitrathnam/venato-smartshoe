40ea8b1735f26dd92a1965f829636cb20c5d0727
mehrmahyar <87127405+mehrmahyar@users.noreply.github.com>
2021-10-04 16:07:42 +0330

    imu

---
+++ SimpleAccelerometer_unfiltered.ino
@@ -0,0 +1,64 @@
+/*
+  Arduino LSM9DS1 - Simple Accelerometer
+
+  this code generates Acceleration signal without having a low pass filter.
+*/
+
+#include <Arduino_LSM9DS1.h>
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+  Serial.println("Started");
+
+  if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+    while (1);
+  }
+
+//  Serial.print("Accelerometer sample rate = ");
+//  Serial.print(IMU.accelerationSampleRate());
+//  Serial.println(" Hz");
+//  Serial.println();
+//  Serial.println("Acceleration in G's");
+//  Serial.println('\t');
+}
+
+void loop() {
+  float x, y, z;
+  float mag;
+  float netmag;
+  float avgmag = 0;
+  int i = 1;
+  
+
+  if (IMU.accelerationAvailable()) {
+    IMU.readAcceleration(x, y, z);
+    mag = (sqrt( x*x + y*y + z*z )) - 1.03;
+    
+    netmag = mag - avgmag;
+
+    Serial.print(netmag);
+    Serial.println('\t');
+    //Serial.print(millis());
+
+    avgmag = (avgmag + mag)/ i;
+    i++;
+    
+    
+  }
+
+  
+  
+
+  
+
+  
+
+  
+   
+
+   
+
+  
+}
